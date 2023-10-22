abfc20d0c000027e6b185b70c6fba6cdcd309dfd
SivaSurender <e.surender14@gmail.com>
2023-10-22 12:20:34 +0530

    Added code

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="border-b border-stone-400 bg-yellow-500 p-3">
-      <Link to="/" className="mt-4 font-extrabold uppercase tracking-widest">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
 
